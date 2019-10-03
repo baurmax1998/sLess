3c34258b88f8d2e750aef534fe640447a94230dd
max <baurmax1998@hotmail.com>
2019-10-03 17:57:01 +0200

    editor .var with primitives works

--- pages/editor_page.js
+++ pages/editor_page.js
@@ -3,130 +3,28 @@ function initEditor(funName) {
   console.log(funName)
   $("#code_editor").show();
 
-  
-
-  var tributeMultipleTriggers = new Tribute({
-    collection: [{
-      // The function that gets call on select that retuns the content to insert
-      selectTemplate: function (item) {
-        if (this.range.isContentEditable(this.current.element)) {
-          return '<a href="http://zurb.com" title="' + item.original.email + '">@' + item.original.value + '</a>';
-        }
-
-        return '@' + item.original.value;
-      },
-
-      // the array of objects
-      values: [
-        { key: 'Jordan Humphreys', value: 'Jordan Humphreys', email: 'jordan@zurb.com' },
-        { key: 'Sir Walter Riley', value: 'Sir Walter Riley', email: 'jordan+riley@zurb.com' }
-      ]
-    }, {
-      // The symbol that starts the lookup
-      trigger: '#',
-
-      // The function that gets call on select that retuns the content to insert
-      selectTemplate: function (item) {
-        if (this.range.isContentEditable(this.current.element)) {
-          return '<a href="mailto:' + item.original.email + '">#' + item.original.name.replace() + '</a>';
-        }
-
-        return '#' + item.original.name;
-      },
-
-      // function retrieving an array of objects
-      values: [
-        { name: 'Bob Bill', email: 'bobbill@example.com' },
-        { name: 'Steve Stevenston', email: 'steve@example.com' }
-      ],
-
-      lookup: 'name',
-
-      fillAttr: 'name'
-    }]
-  });
-
-
-  // example of Tribute in autocomplete mode
-  var tributeAutocompleteTest = new Tribute({
-    autocompleteMode: true,
-    menuContainer: document.getElementById('content'),
-    values: [
-      { key: "Alabama", value: "Alabama" },
-      { key: "Alaska", value: "Alaska" },
-      { key: "Arizona", value: "Arizona" },
-      { key: "Arkansas", value: "Arkansas" },
-      { key: "California", value: "California" },
-      { key: "Colorado", value: "Colorado" },
-      { key: "Connecticut", value: "Connecticut" },
-      { key: "Delaware", value: "Delaware" },
-      { key: "Florida", value: "Florida" },
-      { key: "Georgia", value: "Georgia" },
-      { key: "Hawaii", value: "Hawaii" },
-      { key: "Idaho", value: "Idaho" },
-      { key: "Illinois", value: "Illinois" },
-      { key: "Indiana", value: "Indiana" },
-      { key: "Iowa", value: "Iowa" },
-      { key: "Kansas", value: "Kansas" },
-      { key: "Kentucky", value: "Kentucky" },
-      { key: "Louisiana", value: "Louisiana" },
-      { key: "Maine", value: "Maine" },
-      { key: "Maryland", value: "Maryland" },
-      { key: "Massachusetts", value: "Massachusetts" },
-      { key: "Michigan", value: "Michigan" },
-      { key: "Minnesota", value: "Minnesota" },
-      { key: "Mississippi", value: "Mississippi" },
-      { key: "Missouri", value: "Missouri" },
-      { key: "Montana", value: "Montana" },
-      { key: "Nebraska", value: "Nebraska" },
-      { key: "Nevada", value: "Nevada" },
-      { key: "New Hampshire", value: "New Hampshire" },
-      { key: "New Jersey", value: "New Jersey" },
-      { key: "New Mexico", value: "New Mexico" },
-      { key: "New York", value: "New York" },
-      { key: "North Carolina", value: "North Carolina" },
-      { key: "North Dakota", value: "North Dakota" },
-      { key: "Ohio", value: "Ohio" },
-      { key: "Oklahoma", value: "Oklahoma" },
-      { key: "Oregon", value: "Oregon" },
-      { key: "Pennsylvania", value: "Pennsylvania" },
-      { key: "Rhode Island", value: "Rhode Island" },
-      { key: "South Carolina", value: "South Carolina" },
-      { key: "South Dakota", value: "South Dakota" },
-      { key: "Tennessee", value: "Tennessee" },
-      { key: "Texas", value: "Texas" },
-      { key: "Utah", value: "Utah" },
-      { key: "Vermont", value: "Vermont" },
-      { key: "Virginia", value: "Virginia" },
-      { key: "Washington", value: "Washington" },
-      { key: "West Virginia", value: "West Virginia" },
-      { key: "Wisconsin", value: "Wisconsin" },
-      { key: "Wyoming", value: "Wyoming" }
-    ],
-    selectTemplate: function (item) {
-      if (typeof item === 'undefined') return null;
-      if (this.range.isContentEditable(this.current.element)) {
-        return '<span contenteditable="false"><a>' + item.original.key + '</a></span>';
-      }
-
-      return item.original.value;
-    },
-    menuItemTemplate: function (item) {
-      return item.string;
-    }
-  });
+  var autocomplete = initAutocomplete(funName);
 
   var tributeNoMatch = new Tribute({
-    // trigger: '@',
+    allowSpaces: true,
     autocompleteMode: true,
-    values: function (text, cb) {
-      console.log(text)
-      cb([{
-          name: "bob"
-        },{
-          name: "bob1"
-        }
-      ])
+    values: autocomplete,
+    selectTemplate: function (item) {
+      if (typeof item === 'undefined') return null;
+      setTimeout(function () {
+        var node = activeRow()[0];
+        var range = document.createRange();
+        range.setStart(node, 3);
+        range.setEnd(node, 4);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }, 100);
+      return "var <span>name</span> = 'max'";
+    },
+    menuItemTemplate: function (item) {
+      return "<span class='left'>" + item.string + "</span>"
+        + "<span class='right'>" + item.original.info + "</span>";
     },
     lookup: 'name',
     fillAttr: 'name'
@@ -137,17 +35,50 @@ function initEditor(funName) {
     // tributeAutocompleteTest.attach(item);
     // tributeMultipleTriggers.attach(item);
     tributeNoMatch.attach(item);
-
   })
+}
 
+function initAutocomplete(params) {
+  return function (text, cb) {
+    var textWithoutFunc = text.split(".")[0]
+    var posibilitys = [];
+    posibilitys = addVars(posibilitys, textWithoutFunc);
+    cb(posibilitys)
+  }
 
+  function addVars(posibilitys, textWithoutFunc) {
+    if(!activeRow().text().includes("var")){
+      posibilitys = posibilitys.concat([{
+        name: "true.var",
+        info: "bool",
+        action: "var"
+      }, {
+        name: "false.var",
+        info: "bool",
+        action: "var"
+      }, {
+        name: textWithoutFunc + ".var",
+        info: "string",
+        action: "var"
+      }]);
+      if (!isNaN(textWithoutFunc)) {
+        posibilitys.push({
+          name: textWithoutFunc + ".var",
+          info: "number",
+          action: "var"
+        })
+      }
+    } 
+    return posibilitys;
+  }
+}
+
+function activeRow() {
+  return $("#idContentEditable > div:focus");
 }
 
 
 function tempInit() {
-
-
-
   $("#idContentEditable > div").keydown(function editorKeydown(e) {
     let target = $(e.target);
     if (e.which == 13 && !$(".tribute-container").is(":visible")) {
